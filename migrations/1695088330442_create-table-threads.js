549278d5e810a03db1464fadd265fd0342e33870
areef44 <areef.empat.empat@gmail.com>
2023-09-19 09:16:07 +0700

    create table threads

---
+++ migrations/1695088330442_create-table-threads.js
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+    pgm.createTable('threads',{
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        title: {
+            type: 'VARCHAR(255)',
+            notNull: true,
+        },
+        body: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        owner: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            references: '"users"',
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        }
+    });
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('threads');
+};
