3dd1f3593ec4d3f9a0163d4e9f43bf2b064300c4
areef44 <areef.empat.empat@gmail.com>
2023-09-20 09:30:55 +0700

    add ThreadRepository in Domain Directory

---
+++ src/Domains/threads/_test/ThreadRepository.test.js
@@ -0,0 +1,9 @@
+const ThreadRepository = require('../ThreadRepository');
+
+describe('ThreadRepository Interface', () => {
+    it('should throw error when invoke abstract behavior', async () => {
+        const threadRepository = new ThreadRepository();
+
+        await expect(threadRepository.createThread({})).rejects.toThrowError('THREAD_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    });
+});
