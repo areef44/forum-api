df94f718a01fd076f49ccd52a7883b388cb13b00
areef44 <areef.empat.empat@gmail.com>
2023-09-22 08:51:11 +0700

    create addthread usecase and addthreat usecate test in application directory

---
+++ src/Applications/use_case/CreateThreadUseCase.js
@@ -0,0 +1,14 @@
+const CreateThread = require('../../Domains/threads/entities/CreateThread');
+
+class CreateThreadUseCase {
+    constructor({ threadRepository }) {
+        this._threadRepository = threadRepository
+    }
+
+    async execute(useCasePayload) {
+        const newThread = new CreateThread(useCasePayload);
+        return this._threadRepository.createThread(newThread)
+    }
+}
+
+module.exports = CreateThreadUseCase;
---
+++ src/Applications/use_case/_test/CreateThreadUseCase.test.js
@@ -0,0 +1,41 @@
+const CreatedThreat = require('../../../Domains/threads/entities/CreatedThread');
+const CreateThread = require('../../../Domains/threads/entities/CreateThread');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+const CreateThreadUseCase = require('../CreateThreadUseCase');
+
+describe('CreateThreadUseCase', () => {
+    it('should orchestrating the create thread action correctly', async () =>{
+        // Arrange
+        const useCasePayload = {
+            title: 'sebuah thread',
+            body: 'body sebuah thread',
+            owner: 'user-forum-123',
+        };
+        const expectedCreatedThread = new CreatedThreat({
+            id: 'thread-h_123',
+            title: useCasePayload.title,
+            body: useCasePayload.body,
+            owner: useCasePayload.owner,
+        });
+
+        const mockThreadRepository = new ThreadRepository();
+
+        mockThreadRepository.createThread = jest.fn().mockImplementation(() => Promise.resolve(expectedCreatedThread));
+
+        const getThreadUseCase = new CreateThreadUseCase({
+            threadRepository: mockThreadRepository,
+        });
+
+        // Action
+        const createdThread = await getThreadUseCase.execute(useCasePayload);
+
+        // Assert
+        expect (createdThread).toStrictEqual(expectedCreatedThread);
+        expect(mockThreadRepository.createThread).toBeCalledWith(new CreateThread({
+            title: useCasePayload.title,
+            body: useCasePayload.body,
+            owner: useCasePayload.owner,
+        }));
+
+    });
+});
