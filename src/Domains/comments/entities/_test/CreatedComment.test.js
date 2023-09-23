1a39f6d827b406689c7eb3e009db1178264de19e
areef44 <areef.empat.empat@gmail.com>
2023-09-23 19:33:20 +0700

    add createdcomment.test.js and createdcomment.js in domain entities directories

---
+++ src/Domains/comments/entities/_test/CreatedComment.test.js
@@ -0,0 +1,43 @@
+const CreatedComment = require('../CreatedComment');
+
+describe ('CreatedComment entities', () => {
+    it('should throw error when payload did not contain needed property', () => {
+        // Arrange
+        const payload = {
+            content: 'sebuah comment',
+            owner: 'userforum-123',
+        }
+
+        // Action & Assert
+        expect(() => new CreatedComment(payload)).toThrowError('CREATED_COMMENT.NOT_CONTAIN_NEEDED_PROPERTY');
+    });
+
+    it('should throw error when payload did not meet data type specification', () => {
+        // Arrange
+        const payload = {
+            id: 123,
+            content: 'sebuah comment',
+            owner: false,
+        };
+
+        // Action & Assert
+        expect(() => new CreatedComment(payload)).toThrowError('CREATED_COMMENT.NOT_MEET_DATA_TYPE_SPECIFICATION');
+    });
+
+    it('should create new comment object correctly', () => {
+        // Arrange
+        const payload = {
+            id: 'comment-_user123',
+            content: 'sebuah comment',
+            owner: 'dicoding',
+        }
+
+        // Action
+        const createdComment = new CreatedComment(payload);
+
+        // Assert
+        expect(createdComment.id).toEqual(payload.id);
+        expect(createdComment.content).toEqual(payload.content);
+        expect(createdComment.owner).toEqual(payload.owner);
+    });
+});
