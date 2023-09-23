e53ac9a7fe50d583a7e433d37744fb6d394520f5
areef44 <areef.empat.empat@gmail.com>
2023-09-23 21:15:46 +0700

    add handler.js routes.js and index.js for comments

---
+++ src/Interfaces/http/api/comments/handler.js
@@ -0,0 +1,30 @@
+const CreateCommentUseCase = require("../../../../Applications/use_case/CreateCommentUseCase");
+
+class CommentHandler {
+    constructor(container) {
+        this._container = container;
+    }
+
+    async postCommentHandler(request, h) {
+        const createCommentUseCase = this._container.getInstance(CreateCommentUseCase.name);
+        const { id : owner } = request.auth.credentials;
+        const thread = request.params.threadId;
+        const useCasePayload = {
+            content: request.payload.content,
+            thread,
+            owner,
+        };
+        const addedComment = await createCommentUseCase.execute(useCasePayload);
+
+        const response = h.response({
+            status: 'success',
+            data: {
+                addedComment,
+            },
+        });
+        response.code(201);
+        return response;
+    }
+}
+
+module.exports = CommentHandler;
