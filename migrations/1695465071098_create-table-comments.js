5bff3a5ff89622d290b9ccc2998d53ebb9f32d8e
areef44 <areef.empat.empat@gmail.com>
2023-09-23 17:37:01 +0700

    add migrations table comments

---
+++ migrations/1695465071098_create-table-comments.js
@@ -0,0 +1,41 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+    pgm.createTable('comments',{
+        id: {
+            type: 'VARCHAR(50)',
+            primaryKey: true,
+        },
+        thread: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            references: '"threads"',
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+        },
+        content: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        owner: {
+            type: 'VARCHAR(50)',
+            notNull: true,
+            references: '"users"',
+            onDelete: 'cascade',
+            onUpdate: 'cascade',
+        },
+        created_at: {
+            type: 'TEXT',
+            notNull: true,
+        },
+        updated_at: {
+            type: 'TEXT',
+            notNull: true,
+        }
+    })
+};
+
+exports.down = (pgm) => {
+    pgm.dropTable('comments');
+};
+
