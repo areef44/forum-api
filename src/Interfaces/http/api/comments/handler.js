c5de3ac66dfb2f89f5035d6b65bf58fd88895d04
areef44 <areef.empat.empat@gmail.com>
2023-09-24 08:22:31 +0700

    add delete comment in handler and routes of comment repository

--- src/Interfaces/http/api/comments/handler.js
+++ src/Interfaces/http/api/comments/handler.js
@@ -1,4 +1,5 @@
 const CreateCommentUseCase = require("../../../../Applications/use_case/CreateCommentUseCase");
+const DeleteCommentUseCase = require('../../../../Applications/use_case/DeleteCommentUseCase');
 
 class CommentHandler {
     constructor(container) {
@@ -27,6 +28,27 @@ class CommentHandler {
         response.code(201);
         return response;
     }
+
+    async deleteCommentHandler(request, h) {
+        const deleteCommentUseCase = this._container.getInstance(DeleteCommentUseCase.name);
+        const { id: owner } = request.auth.credentials;
+        const thread = request.params.threadId;
+        const comment = request.params.commentId;
+        const useCasePayload = {
+            thread,
+            comment,
+            owner,
+        };
+        await deleteCommentUseCase.execute(useCasePayload);
+
+        const response = h.response({
+            status: 'success',
+        });
+        response.code(200);
+        return response;
+    }
 }
+
+module.exports = CommentHandler;
 
 module.exports = CommentHandler;
