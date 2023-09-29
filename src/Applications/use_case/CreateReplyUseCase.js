0e11622b0220478a95198fb1cd6565d8513fd3ac
areef44 <areef.empat.empat@gmail.com>
2023-09-29 20:01:00 +0700

    add create reply use case and test

---
+++ src/Applications/use_case/CreateReplyUseCase.js
@@ -0,0 +1,19 @@
+const CreateReply = require('../../Domains/replies/entities/CreateReply');
+
+
+class CreateReplyUseCase {
+    constructor({ threadRepository, commentRepository, replyRepository }) {
+        this._threadRepository = threadRepository;
+        this._commentRepository = commentRepository;
+        this._replyRepository = replyRepository;
+    }
+
+    async execute(useCasePayload) {
+        await this._threadRepository.getDetailThread(useCasePayload.threadId);
+        await this._commentRepository.checkAvailabilityComment(useCasePayload.comment);
+        const createReply = new CreateReply(useCasePayload);
+        return this._replyRepository.createReply(createReply);
+    }
+}
+
+module.exports = CreateReplyUseCase;
---
+++ src/Applications/use_case/_test/CreateReplyUseCase.test.js
@@ -0,0 +1,57 @@
+const CreateReplyUseCase = require('../CreateReplyUseCase');
+const CreateReply = require('../../../Domains/replies/entities/CreateReply');
+const CreatedReply = require('../../../Domains/replies/entities/CreatedReply');
+const ReplyRepository = require('../../../Domains/replies/ReplyRepository');
+const CommentRepository = require('../../../Domains/comments/CommentRepository');
+const ThreadRepository = require('../../../Domains/threads/ThreadRepository');
+
+describe('CreateReplyUseCase', () => {
+  /**
+   * Menguji apakah use case mampu mengoskestrasikan langkah demi langkah dengan benar.
+   */
+  it('should orchestrating the create reply action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      content: 'First reply',
+      owner: 'user-123',
+      comment: 'comment-123',
+    };
+    const expectedCreatedReply = new CreatedReply({
+        id: 'reply-123',
+        content: useCasePayload.content,
+        owner: useCasePayload.owner,
+    });
+    
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockReplyRepository = new ReplyRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.getDetailThread = jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.checkAvailabilityComment= jest.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockReplyRepository.createReply = jest.fn()
+      .mockImplementation(() => Promise.resolve(expectedCreatedReply));
+
+    /** creating use case instance */
+    const createReplyUseCase = new CreateReplyUseCase({
+      threadRepository: mockThreadRepository,
+      commentRepository: mockCommentRepository,
+      replyRepository: mockReplyRepository,
+    });
+
+    // Action
+    const createdReply = await createReplyUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(createdReply).toStrictEqual(expectedCreatedReply);
+    expect(mockCommentRepository.checkAvailabilityComment).toBeCalledWith(useCasePayload.comment);
+    expect(mockReplyRepository.createReply).toBeCalledWith(new CreateReply({
+      content: useCasePayload.content,
+      owner: useCasePayload.owner,
+      comment: useCasePayload.comment,
+    }));
+  });
+});
