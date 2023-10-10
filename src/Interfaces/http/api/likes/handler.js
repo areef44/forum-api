7d827f334dedc564961380aaf9d6b1a77f97f109
areef44 <areef.empat.empat@gmail.com>
2023-10-10 22:13:05 +0700

    add interface likes

---
+++ src/Interfaces/http/api/likes/handler.js
@@ -0,0 +1,25 @@
+const CreateDeleteLikeUseCase = require('../../../../Applications/use_case/CreateDeleteLikeUseCase');
+
+class LikesHandler {
+  constructor(container) {
+    this._container = container;
+
+    this.putLikeHandler = this.putLikeHandler.bind(this);
+  }
+
+  async putLikeHandler(request, h) {
+    const { id: userId } = request.auth.credentials;
+    const { threadId, commentId } = request.params;
+    const createDeleteLikeUseCase = this._container.getInstance(CreateDeleteLikeUseCase.name);
+    await createDeleteLikeUseCase.execute({
+      owner: userId, 
+      threadId, commentId,
+    });
+
+    const response = h.response({ status: 'success' });
+    response.code(200);
+    return response;
+  }
+}
+
+module.exports = LikesHandler;
---
+++ src/Interfaces/http/api/likes/routes.js
@@ -0,0 +1,10 @@
+const routes = (handler) => ([
+    {
+      method: 'PUT',
+      path: '/threads/{threadId}/comments/{commentId}/likes',
+      handler: handler.putLikeHandler,
+      options: { auth: 'forumapi_auth' },
+    },
+  ]);
+  
+  module.exports = routes;
